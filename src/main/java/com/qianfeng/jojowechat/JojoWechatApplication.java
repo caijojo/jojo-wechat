45f0963e59a290083060d8f38b9d3312337b2dd0
Cai JOJO <729034301@qq.com>
2018-12-09 19:56:34 +0800

    Initial commit

---
+++ src/main/java/com/qianfeng/jojowechat/JojoWechatApplication.java
@@ -0,0 +1,12 @@
+package com.qianfeng.jojowechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JojoWechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JojoWechatApplication.class, args);
+	}
+}
---
+++ src/test/java/com/qianfeng/jojowechat/JojoWechatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qianfeng.jojowechat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JojoWechatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
